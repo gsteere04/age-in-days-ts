5719941fdda1a993e50dd0160f79610342ba231a
Grant <gsteere4717@xmail.dixietech.edu>
2024-07-16 11:23:45 -0600

    create calculator and app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css';
+import Calculator from './Calculator';
 
 function App() {
   return (
     <>
-
+    <header>
+      <h1>Age in Days Calculator</h1>
+      <p>This app will calculate your days lived, given your years lived!</p>
+    </header>
+    <main>
+      <Calculator />
+    </main>
+    <footer>
+      Created by Grant Steere
+    </footer>
+    
     </>
   );
 }
---
+++ src/Calculator.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Calculator: React.FC = () => {
+    return (
+        <>
+
+        </>
+    );
+}
+export default Calculator;
